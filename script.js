fc795f46ef97fac3913ca8065e5e9ca53ea935ec
Naod Tadele <naodtadele1@gmail.com>
2021-12-02 12:17:26 +0300

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+function showNav(){
+    var nav = document.getElementById("navlists");
+    if(nav.style.display != "none"){
+        nav.style.display = "none";
+    }
+    else{
+        nav.style.display = "flex";
+    }
+}
+
